3b14474d38226c0d2ec2a77a039fe8dae68046ce
king klaus <muigakiduyu1995@gmail.com>
2025-06-02 03:39:22 +0300

    Update disable-services.ps1

--- disable-services.ps1
+++ disable-services.ps1
@@ -2,51 +2,96 @@
 # Disables unnecessary services found in All_Services_List_disabled.txt
 
 $services = @(
-    "AdvancedSystemCareService18"
-    "CCleanerPerformanceOptimizerService"
-    "CyberGhost8Service"
-    "bdvpnservice"
-    "bdredline_agent"
-    "ProductAgentService"
-    "BluetoothUserService_64bf9"
-    "bthserv"
-    "WFDSConMgrSvc"
-    "upnphost"
-    "SSDPSRV"
-    "NetPipeActivator"
-    "NetTcpActivator"
-    "RasAuto"
-    "diagsvc"
-    "DPS"
-    "SDRSVC"
-    "WdiServiceHost"
-    "WdiSystemHost"
-    "wercplsupport"
-    "Sense"
-    "svsvc"
-    "com.docker.service"
-    "VBoxSDS"
-    "VSInstallerElevationService"
-    "VSStandardCollectorService150"
-    "OneSyncSvc_64bf9"
-    "UnistoreSvc_64bf9"
-    "UserDataSvc_64bf9"
-    "CDPSvc"
-    "CDPUserSvc_64bf9"
-    "BITS"
-    "AppReadiness"
-    "AppIDSvc"
-    "MessagingService_64bf9"
-    "NlaSvc"
-    "GoogleChromeElevationService"
-    "GoogleUpdaterInternalService135.0.7023.5"
-    "GoogleUpdaterInternalService137.0.7115.0"
-    "GoogleUpdaterInternalService138.0.7156.2"
-    "GoogleUpdaterInternalService138.0.7194.0"
-    "GoogleUpdaterService135.0.7023.5"
-    "GoogleUpdaterService137.0.7115.0"
-    "GoogleUpdaterService138.0.7156.2"
-    "GoogleUpdaterService138.0.7194.0"
+    
+    "AdvancedSystemCareService18"  # Service from IObit for system optimization
+
+    "CCleanerPerformanceOptimizerService"  # Service from CCleaner for performance enhancement
+
+    "CyberGhost8Service"  # VPN service from CyberGhost
+
+    "bdvpnservice"  # Bitdefender VPN service
+
+    "bdredline_agent"  # Bitdefender service for threat detection
+
+    "ProductAgentService"  # Service associated with product updates and management for installed software
+
+    "BluetoothUserService_64bf9"  # Bluetooth user service for managing Bluetooth devices
+
+    "bthserv"  # Bluetooth Support Service
+
+    "WFDSConMgrSvc"  # Windows Firewall Device Control Manager service
+
+    "upnphost"  # Universal Plug and Play (UPnP) Host service
+
+    "SSDPSRV"  # Simple Service Discovery Protocol service for network discovery of devices
+
+    "NetPipeActivator"  # Net Pipe activation service, related to .NET remoting
+
+    "NetTcpActivator"  # TCP activation service used for .NET applications
+
+    "RasAuto"  # Remote Access Auto Connection Manager
+
+    "diagsvc"  # Diagnostic Execution service for Windows diagnostics
+
+    "DPS"  # Device Parameter Service
+
+    "SDRSVC"  # Windows Backup service for system recovery and restoration
+
+    "WdiServiceHost"  # Windows Diagnostic Infrastructure service host
+
+    "WdiSystemHost"  # System host for Windows Diagnostic Infrastructure
+
+    "wercplsupport"  # Windows Error Reporting Control Panel Support service
+
+    "Sense"  # Windows sensing service for adaptive background functions
+
+    "svsvc"  # An unknown service that might relate to various applications or features
+
+    "com.docker.service"  # Docker service for container management
+
+    "VBoxSDS"  # VirtualBox shared folder service
+
+    "VSInstallerElevationService"  # Visual Studio Installer service for elevation management during installations
+
+    "VSStandardCollectorService150"  # Visual Studio standard data collection service
+
+    "OneSyncSvc_64bf9"  # Microsoft OneSync synchronization service for account data
+
+    "UnistoreSvc_64bf9"  # User Store service for managing user data
+
+    "UserDataSvc_64bf9"  # User Data Access service for applications
+
+    "CDPSvc"  # Connected Devices Platform service for device connectivity
+
+    "CDPUserSvc_64bf9"  # User service for Connected Devices Platform
+
+    "BITS"  # Background Intelligent Transfer Service, used for background downloading
+
+    "AppReadiness"  # Service for application readiness during startup or installation
+
+    "AppIDSvc"  # Application Identity service for app verification in enterprise settings
+
+    "MessagingService_64bf9"  # Messaging service for handling user messaging applications
+
+    "NlaSvc"  # Network Location Awareness service for network configuration and management
+
+    "GoogleChromeElevationService"  # Service for elevating privileges for Google Chrome processes
+
+    "GoogleUpdaterInternalService135.0.7023.5"  # Internal Google Updater service for version 135.0.7023.5
+
+    "GoogleUpdaterInternalService137.0.7115.0"  # Internal Google Updater service for version 137.0.7115.0
+
+    "GoogleUpdaterInternalService138.0.7156.2"  # Internal Google Updater service for version 138.0.7156.2
+
+    "GoogleUpdaterInternalService138.0.7194.0"  # Internal Google Updater service for version 138.0.7194.0
+
+    "GoogleUpdaterService135.0.7023.5"  # Google Updater service for version 135.0.7023.5
+
+    "GoogleUpdaterService137.0.7115.0"  # Google Updater service for version 137.0.7115.0
+
+    "GoogleUpdaterService138.0.7156.2"  # Google Updater service for version 138.0.7156.2
+
+    "GoogleUpdaterService138.0.7194.0"  # Google Updater service for version 138.0.7194.0
 )
 
 $logFile = ".\disabled_services_log.txt"
